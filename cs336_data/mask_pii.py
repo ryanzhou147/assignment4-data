1126d15fc008430cb38cfac9e48d91c373f0d05f
Ryan Zhou <ryanawesome888@gmail.com>
2025-12-24 23:13:44 -0800

    Wrote function to mask ip addresses

--- cs336_data/mask_pii.py
+++ cs336_data/mask_pii.py
@@ -8,3 +8,7 @@ def mask_email(text: str, pattern: str = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-
 def mask_phone_number(text: str, pattern: str = r"\(?\d{3}\)?[\s.\-]?\d{3}[\s.\-]?\d{4}") -> tuple[str, int]:
     masked_text, num_subs = re.subn(pattern, "|||PHONE_NUMBER|||", text)
     return masked_text, num_subs
+
+def mask_ip_address(text: str, pattern: str = r"\b(?:\d{1,3}\.){3}\d{1,3}\b") -> tuple[str, int]:
+    masked_text, num_subs = re.subn(pattern, "|||IP_ADDRESS|||", text)
+    return masked_text, num_subs
